fc12cdd0bafe5eca9841deecdc41f0db74ba7911
Steven Magallanes <stevenmagallanes@gmail.com>
2020-01-06 20:50:22 +0000

    Finished following a tutorial for constructing a unique architecture of sub-Angular apps inside IFrames.

--- Startup.cs
+++ Startup.cs
@@ -62,7 +62,8 @@ namespace tbr_dnc_ng
 
                 if (env.IsDevelopment())
                 {
-                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseAngularCliServer(npmScript: "start");
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
